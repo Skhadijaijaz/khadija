2df7acbffd80e296ddfe41a9a9564c5a8420b642
Skhadijaijaz <43703393+Skhadijaijaz@users.noreply.github.com>
2018-12-08 09:50:43 +0500

    commit all

---
+++ bookyourdoctor/Controllers/adminsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bookyourdoctor;
+
+namespace bookyourdoctor.Controllers
+{
+    public class adminsController : Controller
+    {
+        private FINALSCRIPTTEntities db = new FINALSCRIPTTEntities();
+
+        // GET: admins
+        public ActionResult Index()
+        {
+            return View(db.admins.ToList());
+        }
+
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+        public ActionResult DoctorInformation()
+        {
+            return View(db.doctors.ToList());
+        }
+        public ActionResult PatientInformation()
+        {
+
+            return View(db.patients.ToList());
+        }
+        public ActionResult AddDoctor()
+        {
+            return View();
+        }
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+        public ActionResult UpdateDoctor()
+        {
+            return View();
+        }
+        public ActionResult DeleteDoctor()
+        {
+            return View();
+        }
+        public ActionResult Reset()
+        {
+            return View();
+        }
+
+
+        // GET: admins/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // GET: admins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: admins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "email,password")] admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.admins.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(admin);
+        }
+
+        // GET: admins/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: admins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "email,password")] admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        // GET: admins/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            admin admin = db.admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            admin admin = db.admins.Find(id);
+            db.admins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
